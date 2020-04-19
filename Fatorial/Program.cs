bde6a266775848b4cbe591f06ceb9568b04d0f46
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-04-19 15:17:39 -0300

    Add project files.

---
+++ Fatorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número para calcular o seu fatorial: ");
+            int x = int.Parse(Console.ReadLine());
+
+            int resultado = fatorial(x);
+
+            Console.WriteLine(resultado);
+        }
+
+        static int fatorial(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n <= 1)
+                return n;
+            else
+                return n * fatorial(n - 1);
+        }
+    }
+}
